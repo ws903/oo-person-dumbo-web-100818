5a4950b6cda2fdd1c95a58b173865cb61423dced
ws903 <ws903@nyu.edu>
2018-10-10 18:04:29 -0400

    oo-person working

--- lib/person.rb
+++ lib/person.rb
@@ -20,6 +20,16 @@ class Person
 		end
 	end
 
+	def happiness=(user_num)
+		if user_num > 10
+			@happiness = 10
+		elsif user_num < 0
+			@happiness = 0
+		else
+			@happiness = user_num
+		end
+	end
+
 	def hygiene
 		if @hygiene > 10
 			@hygiene = 10
@@ -33,6 +43,8 @@ class Person
 	def hygiene=(user_num)
 		if user_num > 10
 			@hygiene = 10
+		elsif user_num < 0
+			@hygiene = 0
 		else
 			@hygiene = user_num
 		end
@@ -61,6 +73,36 @@ class Person
 
 	def take_bath
 		@hygiene += 4
+		self.hygiene=(@hygiene)
 		return "♪ Rub-a-dub just relaxing in the tub ♫"
+	end
+
+	def work_out
+		@hygiene -= 3
+		@happiness += 2
+		self.hygiene=(@hygiene)
+		self.happiness=(@happiness)
+		return "♪ another one bites the dust ♫"
+	end
+
+	def call_friend(friend)
+		@happiness += 3
+		friend.happiness += 3
+		self.happiness=(@happiness)
+		return "Hi #{friend.name}! It's #{@name}. How are you?"
+	end
+
+	def start_conversation(friend, topic)
+		if topic.downcase == "politics"
+			friend.happiness -= 2
+			self.happiness -= 2
+			return "blah blah partisan blah lobbyist"
+		elsif topic.downcase == "weather"
+			friend.happiness += 1
+			self.happiness += 1
+			return "blah blah sun blah rain"
+		elsif topic.downcase == "programming"
+			return "blah blah blah blah blah"
+		end
 	end
 end
